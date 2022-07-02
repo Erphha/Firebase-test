f5d427f916dac58eb706b1f9ec83a1e19ea9ba1c
Erfan Mougahi <Erphhan@gmail.com>
2022-07-02 18:13:10 +0430

    darkmode

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,17 @@
 import classes from './SignUp.module.css'
 import SignUpForm from './../components/signForm/SignUpForm';
+import {Toggle} from '../components/UI/Toggle'
+import {useDarkMode} from '../styles/useDarkMode'
+import { GlobalStyles } from '../styles/globalStyles';
 
 const SignUp = () => {
+
+    const [theme, toggleTheme] = useDarkMode()
+
     return ( 
         <div className={classes.container}>
+            <GlobalStyles/>
+            <Toggle theme={theme} toggleTheme={toggleTheme}/>
             <SignUpForm/>
         </div>
      );
---
+++ src/styles/globalStyles.jsx
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+.container{
+    background: ${({ theme }) => theme.background};
+    transition: all 0.5s linear;
+}
+.main {
+    background-color: ${({ theme }) => theme.cardBackground};
+  }
+  .sign{
+    color: ${({ theme }) => theme.text}
+  }
+  .username,.email,.password{
+    color: ${({ theme }) => theme.inputColor};
+  background: ${({ theme }) => theme.inputBackgroundColor};
+  }
+  .SignNav{
+    background: ${({ theme }) => theme.navBackgroundColor}
+  }
+  
+`;
+
+export const lightTheme = {
+  background: "#f3ebf6",
+  cardBackground: "#fff;",
+  text: "#8c55aa",
+  inputColor: 'rgb(38, 50, 56)',
+  inputBackgroundColor: 'rgba(136, 126, 126, 0.04)',
+  navBackgroundColor: '#8c55aa'
+};
+
+export const darkTheme = {
+  background: "#373A40",
+  cardBackground: "#909296",
+  text: "#fff",
+  inputColor: 'rgb(254, 255, 255)',
+  inputBackgroundColor: 'rgba(255, 247, 247, 0.299)',
+  navBackgroundColor: '#373A40'
+};
