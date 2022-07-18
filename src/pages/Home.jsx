3465b06ec7c3a88ebf37dfc08d34b26690d0141e
Erfan Mougahi <Erphhan@gmail.com>
2022-07-18 22:50:35 +0430

    Users sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { db, auth } from "../firebase";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import User from '../components/users/User'
+
 const Home = () => {
-    return ( 
-        <>Home Page</>
-     );
-}
- 
+    const [users, setUsers] = useState([])
+  useEffect(() => {
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid]));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+        let users = [];
+        querySnapshot.forEach(doc=>{
+            users.push(doc.data())
+        })
+        setUsers(users);
+    });
+    return ()=> unsub();
+  }, []);
+  console.log(users);
+  return <div className="home-container">
+    <div className="users-container">
+        {users.map(user=><User key={user.uid} user={user} />)}
+    </div>
+  </div>;
+};
+
 export default Home;
